d88e5a09bbd09505812673c5d87e92dd5ac080ce
SamMakuc <124847549+SamMakuc@users.noreply.github.com>
2023-03-22 15:16:10 +0000

    indent

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@
 
 "use strict";
 /**
- * This function calculates area and perimeter of triangle.
- */
+* This function calculates area and perimeter of triangle.
+*/
 function calculate () {
   // input
   let base = parseInt(document.getElementById('base-of-triangle').value);
   let height = parseInt(document.getElementById('height-of-triangle').value);
-
+  
   // process
   let area = (base * height) / 2;
 
